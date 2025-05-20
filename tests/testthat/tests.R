03e851e28c894167b2c817544c222257d6294ac9
Nicolas Escobar <nescoba@iu.edu>
2025-05-20 14:27:23 -0400

    Add tests for Log_Wilks_Lambda and Pillais_Trace functions in ANOVA stats

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,3 +1,28 @@
 testthat("OK", {
     TRUE |> expect_true()
 })
+
+test_that("ANOVA stats work", {
+library(riemtan)
+data("airm")
+  sam1 <- test_pd_mats |>
+    purrr::map(\(x) 2 * x) |>
+    CSample$new(metric_obj = airm)
+  sam2 <- test_pd_mats |> CSample$new(metric_obj = airm)
+  ss <- list(sam1, sam2) |> CSuperSample$new()
+
+ss$Log_Wilks_Lambda() |> (\(x) {
+    list(
+      x |> is.null() |> expect_false(),
+      x |> inherits("numeric") |> expect_true(),
+      x |> expect_lt(0)
+    )
+  })()
+  ss$Pillais_Trace() |> (\(x) {
+    list(
+      x |> is.null() |> expect_false(),
+      x |> inherits("numeric") |> expect_true(),
+      x |> expect_gt(0)
+    )
+  })()
+})
