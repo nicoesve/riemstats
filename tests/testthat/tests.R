d196d222cc39d5a76378e6cfff2adba8f5cf370e
Nicolas Escobar <nicoesve@gmail.com>
2025-05-30 11:29:29 -0400

    Refactor ANOVA tests in tests.R; update formatting and replace commented-out code with active function calls for log_wilks_lambda and pillais_trace

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,28 +1,53 @@
 testthat("OK", {
-    TRUE |> expect_true()
+  TRUE |> expect_true()
 })
 
 test_that("ANOVA stats work", {
-library(riemtan)
-data("airm")
+  library(riemtan)
+  data("airm")
   sam1 <- test_pd_mats |>
     purrr::map(\(x) 2 * x) |>
     CSample$new(metric_obj = airm)
   sam2 <- test_pd_mats |> CSample$new(metric_obj = airm)
   ss <- list(sam1, sam2) |> CSuperSample$new()
 
-ss$Log_Wilks_Lambda() |> (\(x) {
-    list(
-      x |> is.null() |> expect_false(),
-      x |> inherits("numeric") |> expect_true(),
-      x |> expect_lt(0)
-    )
-  })()
-  ss$Pillais_Trace() |> (\(x) {
-    list(
-      x |> is.null() |> expect_false(),
-      x |> inherits("numeric") |> expect_true(),
-      x |> expect_gt(0)
-    )
-  })()
+  # ss$Log_Wilks_Lambda() |> (\(x) {
+  ss |>
+    log_wilks_lambda() |>
+    (\(x){
+      list(
+        x |> is.null() |> expect_false(),
+        x |> inherits("numeric") |> expect_true(),
+        x |> expect_lt(0)
+      )
+    })()
+  ss |>
+    pillais_trace() |>
+    (\(x) {
+      list(
+        x |> is.null() |> expect_false(),
+        x |> inherits("numeric") |> expect_true(),
+        x |> expect_gt(0)
+      )
+    })()
+  ss |>
+    frechet_anova()() |>
+    (\(x) {
+      list(
+        x |> is.null() |> expect_false(),
+        x |> inherits("numeric") |> expect_true(),
+        x |> expect_gt(0),
+        x |> expect_lt(1)
+      )
+    })()
+  ss |>
+    riem_anova() |>
+    (\(x) {
+      list(
+        x |> is.null() |> expect_false(),
+        x |> inherits("numeric") |> expect_true(),
+        x |> expect_gt(0),
+        x |> expect_lt(1)
+      )
+    })()
 })
